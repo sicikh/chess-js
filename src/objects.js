3438637cb9677f5666634609dae14e6ef4c5b4f9
Isenberger <patrick12@riseup.net>
2022-07-01 13:23:40 +0700

    Set .gitignore plus working directory

---
+++ src/objects.js
@@ -0,0 +1,30 @@
+class Figure {
+  constructor(color, type, x, y, image) {
+    this.color = color;
+    this.type = type;
+    this.x = x;
+    this.y = y;
+    this.image = image;
+  }
+
+  getPosition() {
+    const position = {};
+
+    position.x = this.x;
+    position.y = this.y;
+
+    return position;
+  }
+
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+const whiteQueen = new Figure('white', 'queen', 0, 0, 'https://chess.com/whitequeen.image');
+
+console.table(whiteQueen);
+console.log(whiteQueen.getPosition());
+whiteQueen.setPosition(1, 1);
+console.log(whiteQueen.getPosition());
